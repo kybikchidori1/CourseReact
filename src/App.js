b08e857235a9a1625e5c4691f53dca45fbf2db11
Roman <kybik_rybik1994@mail.ru>
2022-07-28 22:12:22 +0300

    end lesson 52

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -24,10 +27,10 @@ function App() {
   ];
   return (
     <div className="App">
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
